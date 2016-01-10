446a68faac3fe1a5105d67c34efde94f9ed35033
Will Lifferth <wlifferth@icloud.com>
2016-01-10 13:28:32 -0500

    updated styles and added otter animation

--- static/js/interaction.js
+++ static/js/interaction.js
@@ -4,3 +4,26 @@ function insertChar(char)
     document.getElementById("search-bar").focus();
     return document.getElementById("search-bar").value;
 }
+
+function otterSwim()
+{
+  otter = document.getElementById("otter");
+  var right = -200;
+  var speed = 1;
+  function updateFrame()
+  {
+    if(right % 30 == 0)
+    {
+      speed = (speed + 1) % 3 + 1;
+    }
+    right = right + speed;
+    otter.style.right = right + 'px';
+
+    if (right >= screen.width)
+    {
+      right = -200;
+    }
+  }
+  var id = setInterval(updateFrame, 30);
+
+}
